52c0293867f92f5f5aee53e814a4f8d4bd6b6301
Stalin Vignesh <stalinvignesh@gmail.com>
2025-12-24 00:13:48 +0000

    Latest with reset jwt

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import hashlib
 import uuid
 from fastapi import FastAPI, HTTPException, Body, Depends, Request, Header
 from fastapi import UploadFile, File, Form , Query
@@ -37,12 +38,15 @@ db = client["weceleb"]
 fs = AsyncIOMotorGridFSBucket(db)
 
 SECRET_KEY = "SECRET_KEY"
+RESET_KEY = "RESET_KEY"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 5
+ACCESS_TOKEN_EXPIRE_MINUTES = 15
 REFRESH_TOKEN_EXPIRE_DAYS = 7
+RESET_TOKEN_EXPIRE_MINUTES = 5
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+oauth2_scheme_reset = OAuth2PasswordBearer(tokenUrl="/reset")
 
 class Token(BaseModel):
     access_token: str
@@ -50,10 +54,18 @@ class Token(BaseModel):
     token_type: str = "bearer"
     message : str
 
+class ResetToken(BaseModel):
+    reset_token: str
+    token_type: str = "bearer"
+    message : str
+
 
 class RefreshToken(BaseModel):
     refresh_token: str
 
+class ResetPin(BaseModel):
+    email : str
+    message : str
 
 # Temporary store for OTPs (in-memory for demo; use DB or Redis in prod)
 otp_store = {}
@@ -79,22 +91,29 @@ def create_access_token(data: dict, expires_delta: timedelta | None = None):
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
+def create_reset_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    expire = datetime.now() + (expires_delta or timedelta(minutes=RESET_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, RESET_KEY, algorithm=ALGORITHM)
+
 @app.post("/send_otp")
 async def send_otp(request: OTPRequest):
     otp = f"{random.randint(1000, 9999)}"
     #otp_store[request.phone_number] = otp
     otp = '1234'
     # Here you should integrate with SMS provider to send the OTP
-    print(f"OTP for {request.phone_number} is {otp}")  # For demo only
+    #print(f"OTP for {request.phone_number} is {otp}")  # For demo only
     return {"message": "OTP sent"}
 
 #@app.post("/verify_otp")
 @app.post("/token", response_model=Token)
 async def token(request: OTPVerifyRequest):
     #expected_otp = otp_store.get(request.phone_number)
-    expected_otp = '1234'
-    if expected_otp != request.otp:
-        raise HTTPException(status_code=400, detail="Invalid OTP")
+    pin = await db["user_data"].find_one({"mobile":request.phone_number},{ "_id" : 0 , "pin" :1 })
+    expected_otp = pwd_context.verify(request.otp,pin["pin"])
+    if not expected_otp:
+        raise HTTPException(status_code=400, detail="Invalid PIN/OTP")
     access_token = create_access_token(data={"sub": request.phone_number}, expires_delta=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES))
     refresh_token = str(uuid.uuid4())
     await db["refresh_tokens"].insert_one({
@@ -104,11 +123,34 @@ async def token(request: OTPVerifyRequest):
     })
     return {"access_token": access_token, "refresh_token": refresh_token , "message" : "success"}
 
+@app.post("/reset",response_model=ResetToken)
+async def reset(request: OTPVerifyRequest ):
+    reset_token = create_reset_token(data={"sub": request.phone_number}, expires_delta=timedelta(minutes=RESET_TOKEN_EXPIRE_MINUTES))
+    await db["refresh_tokens"].insert_one({
+        "token": reset_token,
+        "mobile": request.phone_number,
+        "expires": datetime.now() + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    })
+    return {"reset_token": reset_token,"message" : "success"}
+
+
+
 # Dependency to get current user from token
 async def get_current_user(authorization: str = Header(...)):
     token = authorization.split(" ")[1]  # Bearer <token>
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        phone_number = payload.get("sub")
+        if phone_number is None:
+            raise HTTPException(status_code=401, detail="Invalid token")
+        return phone_number
+    except JWTError:
+        raise HTTPException(status_code=403, detail="Could not validate credentials or expired.")
+
+async def get_current_reset_user(authorization: str = Header(...)):
+    token = authorization.split(" ")[1] # Bearer <token>
+    try:
+        payload = jwt.decode(token, RESET_KEY, algorithms=[ALGORITHM])
         phone_number = payload.get("sub")
         if phone_number is None:
             raise HTTPException(status_code=401, detail="Invalid token")
@@ -132,6 +174,16 @@ async def verify_api_key(authorization: str = Header(...)):
     if authorization != f"Bearer {SECRET_KEY}":
         raise HTTPException(status_code=403, detail="Invalid or missing API key")
 
+@app.post("/reset_pin",response_model=ResetPin)
+async def reset_pin(request: OTPVerifyRequest , _: str = Depends(get_current_reset_user)):
+    pin = request.otp
+    pin_hash = pwd_context.hash(pin)
+    updated = await db["user_data"].update_one({"mobile":request.phone_number},{"$set":{"pin":pin_hash ,
+                                                                                        "is_pin_permanent" : False}})
+    email = await db["user_data"].find_one({"mobile":request.phone_number},{ "_id" : 0 , "email" :1 })
+    if updated.modified_count != 1 :
+        return { "email" : "Error" ,"message": "Error updating DB" }
+    return {"email" : email["email"] ,"message": "Reset Success" }
 
 @app.post("/refresh", response_model=Token)
 async def refresh_token(data : RefreshToken):
@@ -214,18 +266,7 @@ async def delete_by_field(collection: str, query: dict = Body(...), _: str = Dep
     #if result.deleted_count == 0:
     #    raise HTTPException(status_code=404, detail="Delete failed")
     return {"deleted_count" : result.deleted_count}
-'''
-#Need to remove below route
-@app.get("/{collection}/")
-async def get_all_docs(collection: str, _: str = Depends(get_current_user)):
-    cursor = db[collection].find()
-    docs = []
-    async for doc in cursor:
-        doc["id"] = str(doc["_id"])
-        doc.pop("_id", None)
-        docs.append(doc)
-    return docs
-'''
+
 @app.post("/aggregate/{collection}")
 async def aggregate_docs(collection: str,pipeline: List[dict] = Body(...),
     _: str = Depends(get_current_user) ):
